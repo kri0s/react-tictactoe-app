b3df2c6c14a4214455ed757fca7434525c20fd1e
Kayla Lebron <ka022247@ucf.edu>
2020-02-19 11:55:37 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import Game from './components/TicTacToe/index.js'
+
+class Login extends React.Component {
+
+  clickHandler() {
+    console.log("User is logging on");
+    console.log(this);
+    for(const user of this.props.users) {
+      if(this.refs.username.value === user.username && 
+     this.refs.password.value === user.password)
+     this.props.logInUser();
+    }
+  }
+  render() {
+  return <div>
+    <input ref="username" type="text" />
+    <input ref="password" type="password" />
+    <input type="button" value="Login" onClick={() => this.clickHandler()} />
+    </div>;
+}
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {loggedIn:false,
+    users:[{username: "test", password:"test"}]
+   }
+  }
+  logInUser(loggedInStatus) {
+    this.setState({loggedIn:loggedInStatus});
+  }
+  render() {
+    let returnComponent;
+    const login = <Login users={this.state.users}
+    logInUser={() => this.logInUser(true)} />;
+    const game = <Game logOutUser={() => this.logInUser(false)} />;
+    if(!this.state.loggedIn) {
+      returnComponent = login;
+    } else {
+      returnComponent = game;
+    }
+  return returnComponent;
+ }
+}
+
+
+export default App;
